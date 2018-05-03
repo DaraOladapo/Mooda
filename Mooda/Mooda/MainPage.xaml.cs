95876a6d539cf2327127635d943eceb44f14c793
Dara Oladapo <daraoladapo@outlook.com>
2018-05-03 14:13:19 +0100

    Add project files.

---
+++ Mooda/Mooda/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mooda
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
